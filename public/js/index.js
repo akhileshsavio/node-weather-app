93170a327bcf0d70fa4dce019284bd60a7e66d17
akhileshsavio <akhileshsavio252@protonmail.com>
2020-12-21 15:27:19 +0530

    code cahnges for heroku

--- public/js/index.js
+++ public/js/index.js
@@ -21,7 +21,7 @@ const weatherFunction = (e)=>{
     const location = search.value
     messageOne.textContent = 'Loading data....'
     messageTwo.textContent
-    fetch(`http://localhost:3000/weather?address=${location}`).then((resp)=>{
+    fetch(`/weather?address=${location}`).then((resp)=>{
         resp.json().then((data)=>{
             if (data.error){
             messageOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const hbs = require ('hbs')
 const request = require ('postman-request')
 const forecast = require ('./utils/forecast')
 const geocode = require ('./utils/geocode')
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path1.join(__dirname, '../public')
@@ -82,6 +83,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`)
 })
